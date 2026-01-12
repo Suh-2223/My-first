61f5525bfa68df2b6b2259a8bb7408dc1b9e7e17
Suh-2223 <suhanisingh232005@gmail.com>
2026-01-12 19:10:24 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,120 @@
+# name=input("enter your name:")
+# print("hello,world",name)
+# age=int(input("enter your age:"))
+# print("Age is:",age)
+
+#OPERATORS
+#QUES-1
+# a=float(input("enter value of a:"))
+# b=float(input("enter value of b:"))
+# sum=a+b
+# diff=a-b
+# pro=a*b
+# quo=a/b
+# print("the sum is:",sum)
+# print("the difference is:",diff)
+# print("the product is:",pro)
+# print("the quotient is:",quo)
+
+#QUES-2
+# a=15
+# b=4
+# print("the floor division is:",(a//b))
+# print("the remainder is:",(a%b))
+# print("the power is:",(a**b))
+
+#QUES-3
+# n=40
+# p=15
+# tc=(3*p)+(2*n)
+# print("total cost is:",tc)
+
+#QUES-4
+# a=float(input("enter value of a:"))
+# b=float(input("enter value of b:"))
+# c=float(input("enter value of c:"))
+# d=float(input("enter value of d:"))
+# e=float(input("enter value of e:"))
+# f=float(input("enter value of f:"))
+# print("the average is:",(a+b+c+d+e)/5)
+
+#QUES-5
+# n=int(input("enter value of n:"))
+# print("square is:",(n**2))
+# print("cube is:",(n**3))
+# print("square root is:",(n**0.5))
+
+#QUES-6
+# ts=int(input("enter value of ts:"))
+# minutes=ts//60
+# seconds=ts%60
+# print(minutes)
+# print(seconds)
+
+#QUES-7
+# distance=120
+# time=3
+# print("average speed is:",(distance/time))
+
+#QUES-8
+# a=int(input("enter value of a:"))
+# b=int(input("enter value of b:"))
+# if a%b==0:
+#   print(a, "is multiple of",b)
+# else:
+#   print(a ,"is not multiple of", b)
+
+#QUES-9
+# p=int(input("enter value of p:"))
+# r=int(input("enter value of r:"))
+# t=int(input("enter value of t:"))
+# print("the simple interest is:",(p*r*t)/100)
+
+#QUES-10
+# a=float(input("enter value of a:"))
+# b=float(input("enter value of b:"))
+# print("the rounded quotient is:",(a//b))
+
+
+#COMPARISON OPERATORS
+##QUES-1
+# a=int(input("enter value of a:"))
+# b=int(input("enter value of b:"))
+# if a==b:
+#  print(a,"is equal to",b)
+# else:
+#  print(a,"is not equal to",b)
+
+#QUES-2
+# if a>b:
+#   print(a,"is greater than",b)
+
+#QUES-3
+a=int(input("enter value of a:"))
+b=int(input("enter value of b:"))
+c=int(input("enter value of c:"))
+
+if a<b and a<c:
+    print(a,"is smallest")
+elif b<a and b<c:
+    print(b,"is smallest")
+else :
+    print(c,"is smallest")
+
+#QUES-4
+# marks=float(input("enter marks:"))
+# if marks>=40:
+#     print("pass")
+# else:
+#     print("fail")
+
+#QUES-5
+# age=int(input("enter age:"))
+# if age>=18:
+#     print("eligibe to vote")
+# else:
+#     print("not eligible to vote")
+
+
+
+
