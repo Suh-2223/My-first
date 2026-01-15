35958fa3f65953a18c411932602096159b3e6cfc
Suh-2223 <suhanisingh232005@gmail.com>
2026-01-15 23:19:32 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,61 +1,65 @@
 public class Pattern {
     public static void main(String[] args) {
 //    solid rectangle
-// for(int i=1;i<=4;i++){
-//         for(int j=1;j<=5;j++){
-//             System.out.print("* ");
-//         }
-//         System.out.println();
-//     }
-// hollow rectangle
-// for(int i=1;i<=4;i++){
-//         for(int j=1;j<=5;j++){
-//             if(i==1 || i==4 || j==1 || j==5){
-//                 System.out.print("* ");
-//             }
-//             else{
-//                 System.out.print("  ");
-//             }
-//         }
-//         System.out.println();
-//     }
+for(int i=1;i<=4;i++){
+        for(int j=1;j<=5;j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+//hollow rectangle
+for(int i=1;i<=4;i++){
+        for(int j=1;j<=5;j++){
+            if(i==1 || i==4 || j==1 || j==5){
+                System.out.print("* ");
+            }
+            else{
+                System.out.print("  ");
+            }
+        }
+        System.out.println();
+    }
 //pyramid
-// for (int i=1;i<=4;i++) {
-//     for(int j=1;j<=i;j++){
-//         System.out.print("* ");
-//     }
-//     System.out.println();
-// }
+for (int i=1;i<=4;i++) {
+    for(int j=1;j<=i;j++){
+        System.out.print("* ");
+    }
+    System.out.println();
+}
 
-// for (int i=4;i>=1;i--) {
-//     for(int j=1;j<=i;j++){
-//         System.out.print("* ");
-//     }
-//     System.out.println();
-// }
+for (int i=4;i>=1;i--) {
+    for(int j=1;j<=i;j++){
+        System.out.print("* ");
+    }
+    System.out.println();
+}
 
 //inverted pyramid
-// int n=4;
-// for(int i=1;i<=n;i++){
-//  for(int j=1;j<=n-i;j++){
-//      System.out.print(" ");
-//  } 
-//  for(int j=1;j<=i;j++){
-//      System.out.print("*");
-//  }
-//     System.out.println();
-// }
+int n=4;
+for(int i=1;i<=n;i++){
+ for(int j=1;j<=n-i;j++){
+     System.out.print(" ");
+ } 
+ for(int j=1;j<=i;j++){
+     System.out.print("*");
+ }
+    System.out.println();
+}
 //number pattern
-// for(int i=1;i<=5;i++){
-//     for(int j=1;j<=i;j++){
-//         System.out.print(j);
-//     }
-//     System.out.println();
-// }
-// for(int i=5;i>=1;i--){
-//     for(int j=1;j<=i;j++){
-//         System.out.print(j);
-//     }
-//     System.out.println();
-// }
+for(int i=1;i<=5;i++){
+    for(int j=1;j<=i;j++){
+        System.out.print(j);
+    }
+    System.out.println();
+}
+for(int i=5;i>=1;i--){
+    for(int j=1;j<=i;j++){
+        System.out.print(j);
+    }
+    System.out.println();
+}
+    }}
+
+        
+
 }}
